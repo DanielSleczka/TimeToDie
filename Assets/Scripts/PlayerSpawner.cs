316ecae724c7d12b3e3c6ac2fd74971a9004eb5b
DanielSleczka <danielsleczkads@gmail.com>
2022-10-06 18:25:42 +0200

    add playerspawner

---
+++ Assets/Scripts/PlayerSpawner.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class PlayerSpawner : MonoBehaviour
+{
+    public static PlayerSpawner instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public GameObject playerPrefab;
+    private GameObject player;
+
+    public GameObject deathEffect;
+
+    public float respawnTime = 5f;
+
+    private void Start()
+    {
+        if(PhotonNetwork.IsConnected)
+        {
+            SpawnPlayer();
+        }
+    }
+
+    public void SpawnPlayer()
+    {
+        Transform spawnPoint = SpawnManager.instance.GetSpawnPoint();
+
+        player = PhotonNetwork.Instantiate(playerPrefab.name, spawnPoint.position, spawnPoint.rotation);
+    }
+
+    public void Die(string damager)
+    {
+        UIController.instance.deathText.text = $"You were killed by {damager}";
+
+        if(player != null)
+        {
+            StartCoroutine(DieCoroutine());
+        }
+    }
+
+
+    public IEnumerator DieCoroutine()
+    {
+        PhotonNetwork.Instantiate(deathEffect.name, player.transform.position, Quaternion.identity);
+
+        PhotonNetwork.Destroy(player);
+        UIController.instance.deathScreen.SetActive(true);
+
+        yield return new WaitForSeconds(respawnTime);
+
+        UIController.instance.deathScreen.SetActive(false);
+
+        SpawnPlayer();
+    }
+}
